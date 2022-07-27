65f6fb5e8b70bbba2684f6de348e7cd4f2ba920a
Samridha Das <samridhadas782001@gmail.com>
2022-07-27 19:19:17 +0530

    File Structure and Routing

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import "antd/dist/antd.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import React from "react";
+import Homepage from "./pages/Homepage";
+import Items from "./pages/Items";
+
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+      <Routes>
+        <Route path='/home' element={<Homepage/>}/>
+        <Route path='/items' element={<Items/>}/>
+      </Routes>
+      </BrowserRouter>
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/pages/Items.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Items = () => {
+  return (
+    <div className='m-5'>Items</div>
+  )
+}
+
+export default Items
