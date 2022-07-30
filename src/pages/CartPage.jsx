01e8565c2db404abec2a89165bae86f337e5f77e
Samridha Das <samridhadas782001@gmail.com>
2022-07-30 13:49:50 +0530

    Bill API

--- src/pages/CartPage.jsx
+++ src/pages/CartPage.jsx
@@ -1,13 +1,17 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import DefaultLayout from "../components/DefaultLayout";
 import { useDispatch, useSelector } from "react-redux";
-import { Table } from "antd";
+import { Button,message, Modal, Table, Form, Select, Input } from "antd";
 import { DeleteOutlined, PlusOutlined, MinusOutlined } from "@ant-design/icons";
+import axios from 'axios';
+import {useNavigate} from 'react-router-dom';
 
 const CartPage = () => {
+  const navigate = useNavigate();
   const dispatch = useDispatch();
   const { cartItems } = useSelector((state) => state.rootReducer);
-
+  const [subTotal, setSubtotal] = useState(0);
+  const [billChargeOpen, setBillChargeOpen] = useState(false);
   const increaseQuantity = (record) => {
     dispatch({
       type: "updateCart",
@@ -23,6 +27,34 @@ const CartPage = () => {
       });
     }
   };
+
+  const onFinish = (values) => {
+    const reqObj = {
+      ...values,
+      subTotal,
+      cartItems,
+      tax:Number(((subTotal * 10) / 100).toFixed(2)),
+      total:Number(subTotal + Number(((subTotal * 10) / 100).toFixed(2))),
+      userId:JSON.parse(localStorage.getItem('pos-user'))._id,
+    }
+    // console.log(reqObj)
+    axios
+      .post("http://localhost:5000/api/bills/charge-bill", reqObj)
+      .then(() => {
+        message.success("Bill Charged Successfully");
+        navigate('/home')
+      })
+      .catch(() => {
+        message.error("Something went wrong");
+      });
+  };
+  useEffect(() => {
+    let temp = 0;
+    cartItems.forEach((item) => {
+      temp = temp + item.price * item.quantity;
+    });
+    setSubtotal(temp);
+  }, [cartItems]);
 
   const columns = [
     {
@@ -75,6 +107,62 @@ const CartPage = () => {
     <DefaultLayout>
       <h3>Cart</h3>
       <Table columns={columns} dataSource={cartItems} bordered />
+      <hr />
+      <div className="d-flex justify-content-end flex-column align-items-end">
+        <div className="subtotal">
+          <h3>
+            Sub Total :<b>{subTotal}$</b>
+          </h3>
+        </div>
+        <Button
+          onClick={() => {
+            setBillChargeOpen(true);
+          }}
+        >
+          Charge Bill
+        </Button>
+      </div>
+
+      <Modal
+        title="Charge bill"
+        visible={billChargeOpen}
+        footer={false}
+        onCancel={() => {
+          setBillChargeOpen(false);
+        }}
+      >
+        <Form layout="vertical" onFinish={onFinish}>
+          <Form.Item name="customerName" label="Customer Name">
+            <Input />
+          </Form.Item>
+          <Form.Item name="customerMobileNumber" label="Phone Number">
+            <Input />
+          </Form.Item>
+          <Form.Item name="paymentMode" label="Payment Mode">
+            <Select>
+              <Select.Option value="cash">Cash</Select.Option>
+              <Select.Option value="card">Card</Select.Option>
+            </Select>
+          </Form.Item>
+          <div className="charge-bill-amount">
+            <h5>
+              SubTotal :<b>{subTotal}</b>
+            </h5>
+            <h5>
+              Tax:<b>{((subTotal * 10) / 100).toFixed(2)}</b>
+            </h5>
+            <hr />
+            <h2>
+              Total:<b>{subTotal + ((subTotal * 10) / 100).toFixed(2)}</b>
+            </h2>
+          </div>
+          <div className="d-flex justify-content-end">
+            <Button htmlType="submit" type="primary">
+              Generate Bill
+            </Button>
+          </div>
+        </Form>
+      </Modal>
     </DefaultLayout>
   );
 };
